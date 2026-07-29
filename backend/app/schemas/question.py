1d60c15753f98ed85b0516b7a3168245c852e28c
jiyoon <yoonjeongin1003@naver.com>
2026-07-29 11:54:06 +0900

    type, score 필드 제거

--- backend/app/schemas/question.py
+++ backend/app/schemas/question.py
@@ -4,12 +4,9 @@ from typing import List
 class OptionSchema(BaseModel):
     id: str
     text: str
-    type: str
-    score: int
 
 class QuestionSchema(BaseModel):
     id: int
     axis: str
     question: str
     options: List[OptionSchema]
-    
